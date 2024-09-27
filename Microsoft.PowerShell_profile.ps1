e497a95fa78c4b2759bc7b43a5779adc7e65c34c
Henri Perämäki <151518540+peramhe@users.noreply.github.com>
2024-09-27 09:22:38 +0300

    Update Microsoft.PowerShell_profile.ps1
    Small improvements and even more panache

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,19 @@
-$ExecutionContext.SessionState.LanguageMode = "FullLanguage"
+<#
+.SYNOPSIS
+PowerShell profile template with panache.
+
+.DESCRIPTION
+This script is intended to be used as a PowerShell profile script. It displays a title and subtitle in a fancy ASCII art style when the PowerShell console is started.
+
+.NOTES
+The title and subtitle are defined as multi-line strings and can be easily customized.
+
+Known issues:
+- If the console window is too small, the title and subtitle may not be displayed correctly.
+
+Author:
+    Henri Perämäki
+#>
 
 $Title = @"
 
@@ -26,14 +41,28 @@ MM88MMM   ,adPPYba,   8b,dPPYba,      88  88  MM88MMM   ,adPPYba,
 "@
 
 # Split the title and subtitle into matrices of characters
-$MatriceTitle = $Title -split "`n" | % {, $_.ToCharArray()}
-$MatriceSubTitle = $Subtitle -split "`n" | % {, $_.ToCharArray()}
+$MatrixTitle = $Title -split "`n" | % {, $_.ToCharArray()}
+$MatrixSubTitle = $Subtitle -split "`n" | % {, $_.ToCharArray()}
+
+$TitleWidth = ($MatrixTitle | %{$_.length} | Measure-Object -Maximum).Maximum
+$SubtitleWidth = ($MatrixSubTitle | %{$_.length} | Measure-Object -Maximum).Maximum
+
+$MinimumWindowSize = @{
+    X = [math]::max($TitleWidth, $SubtitleWidth) + 10
+    Y = $MatrixTitle.Count + $MatrixSubTitle.Count + 10
+}
+
+If ([Console]::WindowWidth -lt $MinimumWindowSize.X -or [Console]::WindowHeight -lt $MinimumWindowSize.Y) {
+    [Console]::SetWindowSize([math]::max([Console]::WindowWidth, $MinimumWindowSize.X), [math]::max([Console]::WindowHeight, $MinimumWindowSize.Y))
+    $HadToResize = $true
+}
+
 
 $Positions = @()
 
-# Generate random positions for each element in $MatriceTitle
-for ($i = 0; $i -lt $MatriceTitle.Count; $i++) {
-    for ($j = 0; $j -lt $MatriceTitle[$i].Length; $j++) {
+# Generate random positions for each element in $MatrixTitle
+for ($i = 0; $i -lt $MatrixTitle.Count; $i++) {
+    for ($j = 0; $j -lt $MatrixTitle[$i].Length; $j++) {
         $Positions += [PSCustomObject]@{ X = $j; Y = $i }
     }
 }
@@ -44,20 +73,39 @@ $Positions = $Positions | Sort-Object { Get-Random }
 # Draw each element in random order
 foreach ($Pos in $Positions) {
     [Console]::SetCursorPosition($Pos.X, $Pos.Y)
-    Write-Host -NoNewline $MatriceTitle[$Pos.Y][$Pos.X]
+    Write-Host -NoNewline $MatrixTitle[$Pos.Y][$Pos.X]
     (Get-Random -Maximum 20) -eq 4 ? [System.Threading.Thread]::Sleep(1) : $null
 }
 
-$SubtitleOffsetY = $MatriceTitle.Count
-$SubtitleOffsetX = [int]((($MatriceTitle | %{$_.length} | Measure-Object -Maximum).Maximum / 2) - (($MatriceSubTitle | %{$_.length} | Measure-Object -Maximum).Maximum / 2))
-
-for ($col = $MatriceSubTitle[0].Length - 1; $col -ge 0; $col--) {
-    for ($row = 0; $row -lt $MatriceSubTitle.Count; $row++) {
-        [Console]::SetCursorPosition($SubtitleOffsetX + $col, $SubtitleOffsetY + $row)
-        Write-Host -NoNewline $MatriceSubTitle[$row][$col]
+$SubtitleOffsetY = $MatrixTitle.Count
+$SubtitleOffsetX = [int](($TitleWidth / 2) - ($SubtitleWidth / 2))
+$Iteration = 0
+"DarkGray", "Gray", "Green" | ForEach-Object {
+    $Iteration++
+    # Direction of the sweep
+    if ($Iteration -eq 1) {
+        $Init = {$script:col = $MatrixSubTitle[0].Length - 1}
+        $Cond = {$script:col -ge 0}
+        $Iter = {$script:col--}
+    } else {
+        $Init = {$script:col = 0}
+        $Cond = {$script:col -lt $MatrixSubTitle[0].Length}
+        $Iter = {$script:col++}
     }
-    [System.Threading.Thread]::Sleep(1)
+    for (&$Init; &$Cond; &$Iter) {
+        $ForegroundColor = $_
+        for ($row = 0; $row -lt $MatrixSubTitle.Count; $row++) {
+            [Console]::SetCursorPosition($SubtitleOffsetX + $col, $SubtitleOffsetY + $row)
+            Write-Host -NoNewline $MatrixSubTitle[$row][$col] -ForegroundColor $ForegroundColor
+        }
+        
+        ($Iteration -eq 1 -and (Get-Random -Maximum 3) -eq 2) ? [System.Threading.Thread]::Sleep(1) : $null
+    }
 }
 
 # Reset cursor position
-[Console]::SetCursorPosition(0, $SubtitleOffsetY + $MatriceSubTitle.Count + 2)
+[Console]::SetCursorPosition(0, $SubtitleOffsetY + $MatrixSubTitle.Count + 2)
+
+If ($HadToResize) {
+    Write-Host "Note: Resized console to fit the title which may cause suboptimal experience."
+}
